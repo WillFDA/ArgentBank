10b34827de5ed9813117b3a1ad2659f955deedcb
William <william.deazevedopro@gmail.com>
2023-06-05 12:01:33 +0200

    Footer intégré

--- ArgentBank-FrontEnd/src/routes/Root.jsx
+++ ArgentBank-FrontEnd/src/routes/Root.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 export const Root = () => {
   return (
     <>
@@ -8,6 +9,7 @@ export const Root = () => {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
